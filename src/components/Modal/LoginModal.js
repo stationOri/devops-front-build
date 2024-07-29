99e2943b5e5a8dbe80bd6e3804d772eb2cec1fc9
01022549653@naver.com <>
2024-07-29 17:14:50 +0900

    fix: 로그인 uri 수정

--- src/components/Modal/LoginModal.js
+++ src/components/Modal/LoginModal.js
@@ -9,7 +9,7 @@ function LoginModal({ loginClose, loginshow }) {
 
   const handleGoToNaverLogin = async () => {
     try {
-      const response = await axios.get("${process.env.REACT_APP_API_URI}/api/login"); // 서버의 네이버 로그인 엔드포인트 호출
+      const response = await axios.get(`${process.env.REACT_APP_API_URI}/api/login`); // 서버의 네이버 로그인 엔드포인트 호출
       const loginUrl = response.data;
       window.location.href = loginUrl; // 리디렉션 URL로 이동
     } catch (error) {
